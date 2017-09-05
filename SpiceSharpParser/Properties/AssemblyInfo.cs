7e58435abcccbd041a3413d117468f17a12b7b2c
svenboulanger <svenboulanger@hotmail.com>
2017-09-05 09:24:43 +0200

    NuGet

--- SpiceSharpParser/Properties/AssemblyInfo.cs
+++ SpiceSharpParser/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpiceSharpParser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SpiceSharp.Parser")]
+[assembly: AssemblyDescription("Netlist parser for SpiceSharp")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpiceSharpParser")]
--- SpiceSharpTransistors/Properties/AssemblyInfo.cs
+++ SpiceSharpTransistors/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpiceSharpTransistors")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BSIM (Berkeley Short-channel IGFET Model) models for SpiceSharp")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpiceSharpTransistors")]
