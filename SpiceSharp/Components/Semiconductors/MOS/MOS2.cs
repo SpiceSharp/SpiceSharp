b2cad6627b2a9861216274b57c6844592e49a6d2
svenboulanger <svenboulanger@hotmail.com>
2017-10-12 16:01:55 +0200

    MOS2 noise added

--- SpiceSharp/Components/Semiconductors/MOS/MOS2.cs
+++ SpiceSharp/Components/Semiconductors/MOS/MOS2.cs
@@ -239,6 +239,16 @@ namespace SpiceSharp.Components
         public int MOS2states { get; private set; }
 
         /// <summary>
+        /// Noise generators
+        /// </summary>
+        public ComponentNoise MOS2noise { get; } = new ComponentNoise(
+            new Noise.NoiseThermal("rd", 0, 4),
+            new Noise.NoiseThermal("rs", 2, 5),
+            new Noise.NoiseThermal("id", 4, 5),
+            new Noise.NoiseGain("1overf", 4, 5)
+            );
+
+        /// <summary>
         /// Constants
         /// </summary>
         private const int MOS2vbd = 0;
@@ -301,6 +311,15 @@ namespace SpiceSharp.Components
                 MOS2sNodePrime = CreateNode(ckt, Name.Grow("#source")).Index;
             else
                 MOS2sNodePrime = MOS2sNode;
+
+            // Setup noise sources
+            MOS2noise.Setup(ckt,
+                MOS2dNode,
+                MOS2gNode,
+                MOS2sNode,
+                MOS2bNode,
+                MOS2dNodePrime,
+                MOS2sNodePrime);
         }
 
         /// <summary>
@@ -1595,6 +1614,25 @@ namespace SpiceSharp.Components
             method.Terr(MOS2states + MOS2qgs, ckt, ref timeStep);
             method.Terr(MOS2states + MOS2qgd, ckt, ref timeStep);
             method.Terr(MOS2states + MOS2qgb, ckt, ref timeStep);
+        }
+        
+        /// <summary>
+        /// Noise calculations
+        /// </summary>
+        /// <param name="ckt">Circuit</param>
+        public override void Noise(Circuit ckt)
+        {
+            var model = Model as MOS2Model;
+            var state = ckt.State;
+            var noise = state.Noise;
+
+            double Kf = model.MOS2fNcoef * Math.Exp(model.MOS2fNexp * Math.Log(Math.Max(Math.Abs(MOS2cd), 1e-38))) / (MOS2w * (MOS2l - 2 * model.MOS2latDiff) * model.MOS2oxideCapFactor * model.MOS2oxideCapFactor);
+
+            MOS2noise.Evaluate(ckt,
+                MOS2drainConductance,
+                MOS2sourceConductance,
+                2.0 / 3.0 * Math.Abs(MOS2gm),
+                Kf / noise.Freq);
         }
     }
 }
