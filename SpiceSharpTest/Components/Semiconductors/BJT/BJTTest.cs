4ac3da00f0022a4375e2726594dcd8b173877303
marcin-golebiowski <xormus@gmail.com>
2017-11-14 00:32:18 +0100

    Code review changes part 1

--- SpiceSharpTest/Components/Semiconductors/BJT/BJTTest.cs
+++ SpiceSharpTest/Components/Semiconductors/BJT/BJTTest.cs
@@ -2,16 +2,15 @@
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpiceSharp;
-using SpiceSharp.Circuits;
-using SpiceSharp.Components;
-using SpiceSharp.IntegrationMethods;
 using SpiceSharp.Simulations;
 using SpiceSharpTest.Utils;
+using SpiceSharpTest.Models;
+using SpiceSharp.Circuits;
 
 namespace SpiceSharpTest.Components.Semiconductors.BJT
 {
     [TestClass]
-    public class BJTTest
+    public class BJTTest : Framework
     {
         [TestMethod]
         public void Emitter_Follower_DC()
@@ -28,96 +27,59 @@ namespace SpiceSharpTest.Components.Semiconductors.BJT
             {
                 foreach (var resistance in resistancesOfResistorConnectedToEmitter)
                 {
-                    double resistorCurrent = 0;
-
                     var emitterVoltage =
                         GetEmitterVoltageFromEmitterFollowerCircuit(
                             baseVoltage,
                             collectorVoltage,
-                            resistance,
-                            out resistorCurrent);
+                            resistance);
 
-                    var V_BE = baseVoltage - emitterVoltage;
-                    Assert.That.AreEqualWithTol(0.7, V_BE, 0, 0.5);
+                    var voltage_base_emitter = baseVoltage - emitterVoltage;
+
+                    Assert.That.AreEqualWithTol(0.7, voltage_base_emitter, 0, 0.5);
                 }
             }
         }
 
-        private static double GetEmitterVoltageFromEmitterFollowerCircuit(double baseVoltage, double collectorVoltage,
-            int resistance, out double resistorCurrent)
+        private double GetEmitterVoltageFromEmitterFollowerCircuit(
+            double baseVoltage, 
+            double collectorVoltage,
+            int resistance)
         {
-            var baseVoltageSource = new Voltagesource(
-                new CircuitIdentifier("V1"),
-                new CircuitIdentifier("V_B"),
-                new CircuitIdentifier("gnd"),
-                baseVoltage);
-
-            var collectorVoltageSource = new Voltagesource(
-                new CircuitIdentifier("V2"),
-                new CircuitIdentifier("V_C"),
-                new CircuitIdentifier("gnd"),
-                collectorVoltage);
-
-            var resistor = new Resistor(new CircuitIdentifier("R"),
-                new CircuitIdentifier("V_E"),
-                new CircuitIdentifier("gnd"),
-                resistance
-            );
-
-            var bjt = new SpiceSharp.Components.BJT(new CircuitIdentifier("Q1"));
-            var model = GetBjtModel();
-            bjt.SetModel(model);
-
-            bjt.Connect(
-                new CircuitIdentifier("V_C"),
-                new CircuitIdentifier("V_B"),
-                new CircuitIdentifier("V_E"),
-                new CircuitIdentifier("gnd"));
-
-            Circuit ckt = new Circuit();
-            ckt.Objects.Add(baseVoltageSource, collectorVoltageSource, resistor, bjt);
-            ckt.Method = new Trapezoidal();
-            ckt.Method.Initialize();
-
-            OP simulation = new OP("Simulation");
-            
-            double voltage = 0;
-            double currrent = 0;
-            simulation.OnExportSimulationData += (object sender, SimulationData data) =>
+            var netlist = GetEmitterFollowerNetlist(baseVoltage, collectorVoltage, resistance);
+            double emitterVoltage = 0;
+            netlist.OnExportSimulationData += (object sender, SimulationData data) =>
             {
-                var emitterVoltage = data.GetVoltage(
-                    new CircuitIdentifier("V_E"), 
-                    new CircuitIdentifier("gnd"));
-
-                currrent = data.Ask(new CircuitIdentifier("R"), "i");
-
-                voltage = emitterVoltage;
+                emitterVoltage = netlist.Exports[0].Extract(data);
             };
 
-            simulation.Circuit = ckt;
-            simulation.SetupAndExecute();
-            resistorCurrent = currrent;
-            return voltage;
+            netlist.Simulate();
+            return emitterVoltage;
         }
 
-        private static BJTModel GetBjtModel()
+        private Netlist GetEmitterFollowerNetlist(double baseVoltage, double collectorVoltage, int resistance)
         {
-            var bjtModelSpecification = ".MODEL mjd44h11 npn" +
-                                        " IS = 1.45468e-14 BF = 135.617 NF = 0.85 VAF = 10" +
-                                        " IKF = 5.15565 ISE = 2.02483e-13 NE = 3.99964 BR = 13.5617" +
-                                        " NR = 0.847424 VAR = 100 IKR = 8.44427 ISC = 1.86663e-13" +
-                                        " NC = 1.00046 RB = 1.35729 IRB = 0.1 RBM = 0.1" +
-                                        " RE = 0.0001 RC = 0.037687 XTB = 0.90331 XTI = 1" +
-                                        " EG = 1.20459 CJE = 3.02297e-09 VJE = 0.649408 MJE = 0.351062" +
-                                        " TF = 2.93022e-09 XTF = 1.5 VTF = 1.00001 ITF = 0.999997" +
-                                        " CJC = 3.0004e-10 VJC = 0.600008 MJC = 0.409966 XCJC = 0.8" +
-                                        " FC = 0.533878 CJS = 0 VJS = 0.75 MJS = 0.5" +
-                                        " TR = 2.73328e-08 PTF = 0 KF = 0 AF = 1";
+            var netlist = Run(
+                  ".MODEL mjd44h11 npn",
+                  "+ IS = 1.45468e-14 BF = 135.617 NF = 0.85 VAF = 10",
+                  "+ IKF = 5.15565 ISE = 2.02483e-13 NE = 3.99964 BR = 13.5617",
+                  "+ NR = 0.847424 VAR = 100 IKR = 8.44427 ISC = 1.86663e-13",
+                  "+ NC = 1.00046 RB = 1.35729 IRB = 0.1 RBM = 0.1",
+                  "+ RE = 0.0001 RC = 0.037687 XTB = 0.90331 XTI = 1",
+                  "+ EG = 1.20459 CJE = 3.02297e-09 VJE = 0.649408 MJE = 0.351062",
+                  "+ TF = 2.93022e-09 XTF = 1.5 VTF = 1.00001 ITF = 0.999997",
+                  "+ CJC = 3.0004e-10 VJC = 0.600008 MJC = 0.409966 XCJC = 0.8",
+                  "+ FC = 0.533878 CJS = 0 VJS = 0.75 MJS = 0.5",
+                  "+ TR = 2.73328e-08 PTF = 0 KF = 0 AF = 1",
+                  $"V1 V_B gnd {baseVoltage}",
+                  $"V2 V_C gnd {collectorVoltage}",
+                  $"R1 V_E gnd {resistance}",
+                  "Q1 V_C V_B V_E 0 mjd44h11",
+                  ".SAVE V(V_E)",
+                  ".OP"
+                  );
 
-            NetlistReader r = new NetlistReader();
-            r.Parse(new MemoryStream(Encoding.UTF8.GetBytes(bjtModelSpecification)));
-            var model = (BJTModel)r.Netlist.Circuit.Objects[new CircuitIdentifier("mjd44h11")];
-            return model;
+            return netlist;
+
         }
     }
 }
