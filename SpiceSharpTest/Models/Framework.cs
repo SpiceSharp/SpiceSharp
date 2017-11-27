ec581a20c30addc1bb7414654429407b0d3660ff
svenboulanger <svenboulanger@hotmail.com>
2017-11-27 10:15:26 +0100

    Updated transient reference

--- SpiceSharpTest/Models/Framework.cs
+++ SpiceSharpTest/Models/Framework.cs
@@ -116,13 +116,15 @@ namespace SpiceSharpTest.Models
         protected void TestTransient(Netlist netlist, double[] reft, double[] refv)
         {
             var interpolation = LinearSpline.Interpolate(reft, refv);
+            int index = 0;
             netlist.OnExportSimulationData += (object sender, SimulationData data) =>
             {
                 double time = data.GetTime();
                 double actual = netlist.Exports[0].Extract(data);
-                double expected = interpolation.Interpolate(time);
-                double tol = Math.Max(Math.Abs(actual), Math.Abs(expected)) * 1e-3 + 1e-6;
+                double expected = refv[index]; // interpolation.Interpolate(time);
+                double tol = Math.Max(Math.Abs(actual), Math.Abs(expected)) * 1e-5 + 1e-9;
                 Assert.AreEqual(expected, actual, tol);
+                index++;
             };
             netlist.Simulate();
         }
