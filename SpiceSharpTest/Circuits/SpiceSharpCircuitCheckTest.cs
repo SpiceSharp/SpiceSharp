04ab27cc579a734aa25337e2aa9c1438a7486f69
svenboulanger <svenboulanger@hotmail.com>
2018-02-27 18:43:20 +0100

    Using common naming convention for testing - 1

---
+++ SpiceSharpTest/Circuits/SpiceSharpCircuitCheckTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpiceSharp;
+using SpiceSharp.Components;
+using SpiceSharp.Circuits;
+using SpiceSharp.Diagnostics;
+
+namespace SpiceSharpTest.Validation
+{
+    [TestClass]
+    public class SpiceSharpCircuitCheckTest
+    {
+
+    }
+}
---
+++ SpiceSharpTest/Circuits/ValidatorTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpiceSharp;
+using SpiceSharp.Components;
+using SpiceSharp.Diagnostics;
+
+namespace SpiceSharpTest.Validation
+{
+    [TestClass]
+    public class ValidatorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(CircuitException))]
+        public void When_GroundNameInvalid_Expect_Exception()
+        {
+            // Verifies that CircuitException is thrown during Check when circuit has a ground node called "GND"
+            var ckt = CreateCircuit("gnd");
+            ckt.Validate();
+        }
+
+        [TestMethod]
+        public void When_GroundNameValid_Expect_NoException()
+        {
+            // Verifies that CircuitException is not thrown during Check when circuit has a ground node called "gnd"
+            var ckt = CreateCircuit("GND");
+            ckt.Validate();
+        }
+
+        [TestMethod]
+        public void When_GroundNameZero_Expect_NoException()
+        {
+            // Verifies that CircuitException is not thrown during Check when circuit has a ground node called "0"
+            var ckt = CreateCircuit("0");
+            ckt.Validate();
+        }
+
+        /// <summary>
+        /// Creates a circuit with a resistor and a voltage source which is connected to IN
+        /// node and a ground node with name <paramref name="groundNodeName"/>
+        /// </summary>
+        /// <param name="groundNodeName"></param>
+        /// <returns></returns>
+        static Circuit CreateCircuit(string groundNodeName)
+        {
+            Circuit ckt = new Circuit();
+            ckt.Objects.Add(
+                new VoltageSource("V1", "IN", groundNodeName, 1.0),
+                new Resistor("R1", "IN", groundNodeName, 1.0e3));
+            return ckt;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CircuitException), "Voltage source loop has been allowed")]
+        public void When_VoltageLoop_Expect_CircuitException()
+        {
+            Circuit ckt = new Circuit();
+            ckt.Objects.Add(
+                new VoltageSource("V1", "A", "0", 1.0),
+                new VoltageSource("V2", "B", "A", 1.0),
+                new VoltageSource("V3", "B", "A", 1.0)
+                );
+            ckt.Validate();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CircuitException), "Voltage source loop has been allowed")]
+        public void When_VoltageLoop2_Expect_CircuitException()
+        {
+            Circuit ckt = new Circuit();
+            ckt.Objects.Add(
+                new VoltageSource("V1", "A", "0", 1.0),
+                new VoltageSource("V2", "A", "B", 1.0),
+                new VoltageSource("V3", "B", "C", 1.0),
+                new VoltageSource("V4", "C", "D", 1.0),
+                new VoltageSource("V5", "D", "E", 1.0),
+                new VoltageSource("V6", "E", "F", 1.0),
+                new VoltageSource("V7", "F", "G", 1.0),
+                new VoltageSource("V8", "G", "H", 1.0),
+                new VoltageSource("V9", "H", "I", 1.0),
+                new VoltageSource("V10", "I", "0", 1.0)
+                );
+            ckt.Validate();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CircuitException), "Floating node not detected")]
+        public void When_FloatingNode_Expect_CircuitException()
+        {
+            Circuit ckt = new Circuit();
+            ckt.Objects.Add(
+                new VoltageSource("V1", "in", "gnd", 1.0),
+                new Capacitor("C1", "in", "out", 1e-12),
+                new Capacitor("C2", "out", "gnd", 1e-12)
+                );
+            ckt.Validate();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CircuitException), "Floating node not detected")]
+        public void When_FloatingNode2_Expect_CircuitException()
+        {
+            Circuit ckt = new Circuit();
+            ckt.Objects.Add(
+                new VoltageSource("V1", "input", "gnd", 1.0),
+                new VoltageControlledVoltageSource("E1", "out", "gnd", "in", "gnd", 2.0),
+                new VoltageControlledVoltageSource("E2", "out2", "gnd", "out", "gnd", 1.0)
+                );
+            ckt.Validate();
+        }
+    }
+}
