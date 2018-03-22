e86672e64298cb4b3c1ed7cd8bfa1e14e696a8e6
svenboulanger <svenboulanger@hotmail.com>
2018-03-22 11:46:48 +0100

    Fixing git issue -3

---
+++ SpiceSharp/Components/Currentsources/VCCS/VoltageControlledCurrentSource.cs
@@ -0,0 +1,56 @@
+﻿using SpiceSharp.Attributes;
+using SpiceSharp.Components.VoltageControlledCurrentsourceBehaviors;
+
+namespace SpiceSharp.Components
+{
+    /// <summary>
+    /// A voltage-controlled current source
+    /// </summary>
+    [Pin(0, "V+"), Pin(1, "V-"), Pin(2, "VC+"), Pin(3, "VC-"), Connected(0, 1)]
+    public class VoltageControlledCurrentSource : Component
+    {
+        /// <summary>
+        /// Private constants
+        /// </summary>
+        [ParameterName("pincount"), ParameterInfo("Number of pins")]
+        public const int VoltageControlledCurrentSourcePinCount = 4;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">The name of the voltage-controlled current source</param>
+        public VoltageControlledCurrentSource(Identifier name)
+            : base(name, VoltageControlledCurrentSourcePinCount)
+        {
+            // Add parameters
+            ParameterSets.Add(new BaseParameters());
+
+            // Add factories
+            Behaviors.Add(typeof(LoadBehavior), () => new LoadBehavior(Name));
+            Behaviors.Add(typeof(FrequencyBehavior), () => new FrequencyBehavior(Name));
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">The name of the voltage-controlled current source</param>
+        /// <param name="pos">The positive node</param>
+        /// <param name="neg">The negative node</param>
+        /// <param name="controlPos">The positive controlling node</param>
+        /// <param name="controlNeg">The negative controlling node</param>
+        /// <param name="gain">The transconductance gain</param>
+        public VoltageControlledCurrentSource(Identifier name, Identifier pos, Identifier neg, Identifier controlPos, Identifier controlNeg, double gain)
+            : base(name, VoltageControlledCurrentSourcePinCount)
+        {
+            // Add parameters
+            ParameterSets.Add(new BaseParameters(gain));
+
+            // Add factories
+            Behaviors.Add(typeof(LoadBehavior), () => new LoadBehavior(Name));
+            Behaviors.Add(typeof(FrequencyBehavior), () => new FrequencyBehavior(Name));
+
+            // Connect
+            Connect(pos, neg, controlPos, controlNeg);
+        }
+    }
+}
