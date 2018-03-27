ba059c2213c9acd8c78e211affeee454779284a8
svenboulanger <svenboulanger@hotmail.com>
2018-03-27 15:19:47 +0200

    Update data for publishing

--- SpiceSharp/Properties/AssemblyInfo.cs
+++ SpiceSharp/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spice#")]
-[assembly: AssemblyDescription("Spice-based circuit simulator written in C#. Uses Math.Net for solving matrix equations.")]
+[assembly: AssemblyDescription("Spice-based circuit simulator written in C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sven Boulanger")]
 [assembly: AssemblyProduct("SpiceSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("SpiceSharpTest")]
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
