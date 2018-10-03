f1a9f2b2b2fbac3d97f88b9558d6535350e51a6a
svenboulanger <svenboulanger@hotmail.com>
2018-10-03 10:31:33 +0200

    Proposal for implicit operator overloading.

--- SpiceSharp/General/Identifier.cs
+++ SpiceSharp/General/Identifier.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace SpiceSharp
 {
@@ -8,6 +10,44 @@ namespace SpiceSharp
     /// <seealso cref="IEquatable{Identifier}" />
     public abstract class Identifier : IEquatable<Identifier>
     {
+        /// <summary>
+        /// Sets the type of the implicit string.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ArgumentException">
+        /// Invalid type.
+        /// or
+        /// Type does not have a valid constructor.
+        /// </exception>
+        public static void SetImplicitStringIdentifierType(Type value)
+        {
+            // Don't use implicit conversion
+            if (value == null)
+            {
+                _defaultStringConverter = name => new StringIdentifier(name);
+                return;
+            }
+
+            // Check the type
+            var info = value.GetTypeInfo();
+            if (!info.IsSubclassOf(typeof(Identifier)))
+                throw new ArgumentException(@"Invalid type.");
+            var ctor = info.GetConstructor(new[] {typeof(string)});
+            if (ctor == null)
+                throw new ArgumentException(@"Type does not have a valid constructor.");
+
+            // Use a compiled lambda expression for maximum performance
+            var inputArg = Expression.Parameter(typeof(string), "name");
+            var expr = Expression.New(ctor, inputArg);
+            var lambda = Expression.Lambda(typeof(Func<string, Identifier>), expr, inputArg);
+            _defaultStringConverter = (Func<string, Identifier>) lambda.Compile();
+        }
+
+        /// <summary>
+        /// The default string converter.
+        /// </summary>
+        private static Func<string, Identifier> _defaultStringConverter = name => new StringIdentifier(name);
+
         /// <summary>
         /// Indicates whether the current object is equal to another object of the same type.
         /// </summary>
@@ -30,9 +70,6 @@ namespace SpiceSharp
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator Identifier(string id)
-        {
-            return new StringIdentifier(id);
-        }
+        public static implicit operator Identifier(string id) => _defaultStringConverter?.Invoke(id);
     }
 }
--- SpiceSharpTest/Circuits/IdentifierTests.cs
+++ SpiceSharpTest/Circuits/IdentifierTests.cs
@@ -9,47 +9,85 @@ namespace SpiceSharpTest.Circuits
     [TestFixture]
     public class IdentifierTests
     {
-        private class CaseInsensitiveComparer : IEqualityComparer<Identifier>
+        protected class CaseInsensitiveIdentifier : Identifier
         {
+            private readonly string _id;
+
             /// <summary>
-            /// Determines whether the specified objects are equal.
+            /// Initializes a new instance of the <see cref="CaseInsensitiveIdentifier"/> class.
             /// </summary>
-            /// <param name="x">The first object of type <paramref name="T" /> to compare.</param>
-            /// <param name="y">The second object of type <paramref name="T" /> to compare.</param>
-            /// <returns>
-            ///   <see langword="true" /> if the specified objects are equal; otherwise, <see langword="false" />.
-            /// </returns>
-            public bool Equals(Identifier x, Identifier y)
+            /// <param name="name">The name.</param>
+            public CaseInsensitiveIdentifier(string name)
             {
-                // Do case insensitive equality checking on the identifiers
-                return StringComparer.OrdinalIgnoreCase.Equals(x?.ToString(), y?.ToString());
+                _id = name;
             }
 
             /// <summary>
             /// Returns a hash code for this instance.
             /// </summary>
-            /// <param name="obj">The object.</param>
             /// <returns>
             /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
             /// </returns>
-            public int GetHashCode(Identifier obj)
+            public override int GetHashCode()
             {
-                // Make sure the hash code is case insensitive
-                return obj.ToString().ToLowerInvariant().GetHashCode();
+                return _id?.ToLowerInvariant().GetHashCode() ?? 0;
             }
-        }
 
+            /// <summary>
+            /// Indicates whether the current object is equal to another object of the same type.
+            /// </summary>
+            /// <param name="other">An object to compare with this object.</param>
+            /// <returns>
+            /// <c>true</c> if the current object is equal to the <paramref name="other" /> parameter; otherwise, <c>false</c>.
+            /// </returns>
+            public override bool Equals(Identifier other)
+            {
+                if (other is CaseInsensitiveIdentifier cii)
+                    return _id.ToLowerInvariant().Equals(cii._id.ToLowerInvariant());
+                return false;
+            }
+
+            /// <summary>
+            /// Clones this identifier.
+            /// </summary>
+            /// <returns>
+            /// The cloned identifier.
+            /// </returns>
+            public override Identifier Clone() => new CaseInsensitiveIdentifier(_id);
+        }
 
         [Test]
         public void When_CaseInsensitiveIdentifier_Expect_MatchedElement()
         {
-            var ckt = new Circuit(new CaseInsensitiveComparer());
-            ckt.Entities.Add(
+            // Make sure strings are converted to the right type
+            Identifier.SetImplicitStringIdentifierType(typeof(CaseInsensitiveIdentifier));
+            var ckt = new Circuit(
                 new Resistor("R1", "a", "b", 1.0e3),
                 new Capacitor("C1", "b", "0", 1e-6)
                 );
 
-            Assert.AreNotEqual(null, ckt.Entities["r1"]);
+            Assert.DoesNotThrow(() =>
+            {
+                var tmp = ckt.Entities["r1"];
+            });
+
+            // Revert for other tests - will go back to StringIdentifier
+            Identifier.SetImplicitStringIdentifierType(null);
+        }
+
+        [Test]
+        public void When_CaseSensitiveIdentifier_Expect_Exception()
+        {
+            // In this case we didn't change the implicit string type
+            var ckt = new Circuit(
+                new Resistor("R1", "a", "b", 1.0e3),
+                new Capacitor("C1", "b", "0", 1e-6)
+            );
+
+            Assert.Throws<KeyNotFoundException>(() =>
+            {
+                var tmp = ckt.Entities["r1"];
+            });
         }
     }
 }
