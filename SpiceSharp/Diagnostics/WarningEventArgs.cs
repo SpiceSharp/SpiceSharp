1bbe65eeffc537da844f1ad92a29dc23348e87c1
svenboulanger <svenboulanger@hotmail.com>
2018-10-18 09:20:33 +0200

    Warning revision.

---
+++ SpiceSharp/Diagnostics/WarningEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpiceSharp
+{
+    /// <summary>
+    /// Event arguments that are used when a warning is generated.
+    /// </summary>
+    /// <seealso cref="EventArgs" />
+    public class WarningEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the warning message.
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WarningEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">The warning message.</param>
+        public WarningEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
