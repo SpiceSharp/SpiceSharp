4af552d6bbdc7877caab3423e7f29ba5492ac84b
Sven Boulanger <svenboulanger@hotmail.com>
2020-11-19 07:50:20 +0100

    Bug fix and update to netcoreapp2.1

--- SpiceSharp/Components/RLC/Resistors/Resistor.cs
+++ SpiceSharp/Components/RLC/Resistors/Resistor.cs
@@ -25,7 +25,7 @@ namespace SpiceSharp.Components
         /// <summary>
         /// Initializes a new instance of the <see cref="Resistor"/> class.
         /// </summary>
-        /// <param name="name">The name of the entity.</param>
+        /// <param name="name">The name of the resistor.</param>
         public Resistor(string name)
             : base(name, ResistorPinCount)
         {
@@ -34,14 +34,28 @@ namespace SpiceSharp.Components
         /// <summary>
         /// Initializes a new instance of the <see cref="Resistor"/> class.
         /// </summary>
-        /// <param name="name">The name of the resistor</param>
-        /// <param name="pos">The positive node</param>
-        /// <param name="neg">The negative node</param>
+        /// <param name="name">The name of the resistor.</param>
+        /// <param name="pos">The positive node.</param>
+        /// <param name="neg">The negative node.</param>
         /// <param name="res">The resistance.</param>
         public Resistor(string name, string pos, string neg, double res)
             : this(name)
         {
             Parameters.Resistance = res;
+            Connect(pos, neg);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Resistor"/> class.
+        /// </summary>
+        /// <param name="name">The name of the resistor.</param>
+        /// <param name="pos">The positive node.</param>
+        /// <param name="neg">The negative node.</param>
+        /// <param name="model">The model name.</param>
+        public Resistor(string name, string pos, string neg, string model)
+            : this(name)
+        {
+            Model = model;
             Connect(pos, neg);
         }
     }
