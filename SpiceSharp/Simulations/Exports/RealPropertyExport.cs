519563f8e9e755454fee0c073f05b9e98cb64ed3
Ben Katz <katzb123@gmail.com>
2023-08-17 10:04:08 -0500

    added more test cases for op, dc, and tran. added more parameters to check v, i, resistance. changed EntityName to EntityPath and made it a IReadOnlyList<string>

--- SpiceSharp/Simulations/Exports/RealPropertyExport.cs
+++ SpiceSharp/Simulations/Exports/RealPropertyExport.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SpiceSharp.Simulations
 {
@@ -9,9 +10,9 @@ namespace SpiceSharp.Simulations
     public class RealPropertyExport : Export<IEventfulSimulation, double>
     {
         /// <summary>
-        /// Gets the name of the entity.
+        /// Gets the path to the name of the entity.
         /// </summary>
-        public string EntityName { get; }
+        public IReadOnlyList<string> EntityPath { get; }
 
         /// <summary>
         /// Gets the name of the property.
@@ -19,38 +20,32 @@ namespace SpiceSharp.Simulations
         public string PropertyName { get; }
 
         /// <summary>
-        /// Gets the name of the entity in a subcircuit. Use an array of strings.
-        /// </summary>
-        public string[] SubcircuitEntityName { get; }
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="RealPropertyExport"/> class.
         /// </summary>
         /// <param name="simulation">The simulation.</param>
-        /// <param name="entityName">The name of the entity.</param>
+        /// <param name="entityPath">The path to the name of the entity.</param>
         /// <param name="propertyName">The name of the property.</param>
-        public RealPropertyExport(IEventfulSimulation simulation, string entityName, string propertyName)
+        public RealPropertyExport(IEventfulSimulation simulation, string entityPath, string propertyName)
             : base(simulation)
         {
-            EntityName = entityName.ThrowIfNull(nameof(entityName));
+            entityPath.ThrowIfNull(nameof(entityPath));
+            EntityPath = new List<string> { entityPath };
             PropertyName = propertyName.ThrowIfNull(nameof(propertyName));
-            SubcircuitEntityName = null;
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RealPropertyExport"/> class.
         /// </summary>
         /// <param name="simulation">The simulation.</param>
-        /// <param name="subcircuitEntityName">The subcircuit list of the entity.</param>
+        /// <param name="entityPath">The subcircuit list of the entity.</param>
         /// <param name="propertyName">The name of the property.</param>
-        public RealPropertyExport(IEventfulSimulation simulation, string[] subcircuitEntityName, string propertyName)
+        public RealPropertyExport(IEventfulSimulation simulation, string[] entityPath, string propertyName)
             : base(simulation)
         {
-            if (subcircuitEntityName == null || subcircuitEntityName.Length == 0)
-                throw new ArgumentException("subcircuitEntityName cannot be null or empty.", nameof(subcircuitEntityName));
-            EntityName = subcircuitEntityName[0];
+            if (entityPath == null || entityPath.Length == 0)
+                throw new ArgumentNullException("entityPath cannot be null or empty.", nameof(entityPath));
+            EntityPath = new List<string>(entityPath);
             PropertyName = propertyName.ThrowIfNull(nameof(propertyName));
-            SubcircuitEntityName = subcircuitEntityName.ThrowIfNull(nameof(subcircuitEntityName));
         }
 
         /// <summary>
@@ -61,19 +56,19 @@ namespace SpiceSharp.Simulations
         protected override void Initialize(object sender, EventArgs e)
         {
             e.ThrowIfNull(nameof(e));
-            if (SubcircuitEntityName == null)//then using entity name
+            if (EntityPath.Count == 1)//then path is entity name
             {
-                var eb = Simulation.EntityBehaviors[EntityName];
+                var eb = Simulation.EntityBehaviors[EntityPath[0]];
                 if (eb != null)
                     Extractor = eb.CreatePropertyGetter<double>(PropertyName);
             }
             else//using subcircuit array of strings
             {
-                string curComponentName = SubcircuitEntityName[0];
+                string curComponentName = EntityPath[0];
                 System.Collections.Generic.IEnumerator<Behaviors.IBehavior> entityEnum = Simulation.EntityBehaviors[curComponentName].GetEnumerator();
-                for (int i = 1; i < SubcircuitEntityName.Length; i++)
+                for (int i = 1; i < EntityPath.Count; i++)
                 {
-                    string nextComponentName = SubcircuitEntityName[i];
+                    string nextComponentName = EntityPath[i];
 
                     entityEnum.MoveNext();
                     Components.Subcircuits.EntitiesBehavior behavior = (SpiceSharp.Components.Subcircuits.EntitiesBehavior)entityEnum.Current;
@@ -82,7 +77,7 @@ namespace SpiceSharp.Simulations
                     {
                         if (bc.Name == nextComponentName)
                         {
-                            if (i == SubcircuitEntityName.Length - 1)//if last name
+                            if (i == EntityPath.Count - 1)//if last name
                             {
                                 Extractor = bc.CreatePropertyGetter<double>(PropertyName);
                                 return;
