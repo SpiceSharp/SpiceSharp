fdc73a507e4392d81b82718df90e967eaca5162e
Sven Boulanger <svenboulanger@hotmail.com>
2024-01-31 13:02:24 +0100

    Fix #219 - missing interface for finding param set

--- SpiceSharp/Parameters/IParameterized.cs
+++ SpiceSharp/Parameters/IParameterized.cs
@@ -1,12 +1,14 @@
-﻿using System;
-
-namespace SpiceSharp.ParameterSets
+﻿namespace SpiceSharp.ParameterSets
 {
     /// <summary>
     /// An interface that describes a class or struct that defines a parameter set
     /// of the specified type.
     /// </summary>
     /// <typeparam name="P">The parameter set type.</typeparam>
+    /// <remarks>
+    /// This interface allows anyone to check whether a class defines a parameter set of the given type, e.g. for
+    /// <see cref="IParameterSetCollection.TryGetParameterSet{P}(out P)"/>.
+    /// </remarks>
     public interface IParameterized<out P> where P : IParameterSet, ICloneable<P>
     {
         /// <summary>
